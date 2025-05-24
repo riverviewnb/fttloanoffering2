d71d9d83945b4d6b13a5f5364a72cbbc1122ca2e
James Wedderburn <jameswedderburn@Jamess-MacBook-Pro.local>
2025-05-24 14:29:54 -0300

    Add 50 custom valid passwords for team

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,11 +7,32 @@ export default function LoginPage() {
   const router = useRouter();
 
   const validPasswords = [
+    // Original numeric passwords
     '83517', '10492', '67283', '29815', '93047',
     '41602', '38194', '75028', '62901', '51037',
     '20813', '97640', '34729', '59302', '80467',
     '14496', '90238', '33874', '26091', '79105',
     '62539', '43718', '84096', '71520', '31084',
+
+    // mike###
+    'mike475', 'mike823', 'mike129', 'mike638', 'mike204',
+    'mike350', 'mike777', 'mike991', 'mike442', 'mike615',
+
+    // will###
+    'will899', 'will523', 'will741', 'will368', 'will157',
+    'will290', 'will884', 'will119', 'will630', 'will774',
+
+    // anthony###
+    'anthony434', 'anthony287', 'anthony901', 'anthony612', 'anthony378',
+    'anthony865', 'anthony199', 'anthony743', 'anthony058', 'anthony320',
+
+    // james###
+    'james143', 'james589', 'james420', 'james731', 'james204',
+    'james356', 'james002', 'james648', 'james771', 'james908',
+
+    // rick###
+    'rick333', 'rick781', 'rick090', 'rick244', 'rick501',
+    'rick607', 'rick899', 'rick123', 'rick415', 'rick662',
   ];
 
   const handleSubmit = async (e: React.FormEvent) => {
